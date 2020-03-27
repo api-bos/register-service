dfbaceea6348cc13bc5ffff61ca4f3ca0a1012b0
lokox <lokonantachristian@gmail.com>
2020-03-27 11:51:46 +0700

    Update token

--- src/main/java/com/bos/register/config/twillio/TwilioUtil.java
+++ src/main/java/com/bos/register/config/twillio/TwilioUtil.java
@@ -3,6 +3,6 @@ package com.bos.register.config.twillio;
 public class TwilioUtil {
     public static final String ACCOUNT_SID = "AC17cb2fb0f7fd9bfe9b4b619d19b79031";
 
-    public static final String AUTH_TOKEN = "22daeb191e619576cf82a4be1cebdb19";
+    public static final String AUTH_TOKEN = "d870e4bc45d008fccf6a3678fb63aae5";
 }
 
