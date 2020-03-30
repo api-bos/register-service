b45b3a6c17b7c0155977aa8c45bd938ecd34caef
christian lokonanta <lokonantachristian@gmail.com>
2020-03-30 10:35:33 +0700

    Build jar

--- src/main/java/com/bos/register/config/twillio/TwilioUtil.java
+++ src/main/java/com/bos/register/config/twillio/TwilioUtil.java
@@ -3,5 +3,5 @@ package com.bos.register.config.twillio;
 public class TwilioUtil {
     public static final String ACCOUNT_SID = "AC17cb2fb0f7fd9bfe9b4b619d19b79031";
 
-    public static final String AUTH_TOKEN = "ad57b9d17b606818d600a1eb64e55dcc ";
+    public static final String AUTH_TOKEN = "ad57b9d17b606818d600a1eb64e55dcc";
 }
